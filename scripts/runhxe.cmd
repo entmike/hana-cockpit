2e983a41198b70ab039b8ab7085517561d72a1ef
Mike <entmike@gmail.com>
2019-07-24 16:12:34 -0500

    Update windows .cmd w XSC port

---
+++ scripts/runhxe.cmd
@@ -0,0 +1,10 @@
+docker run --rm --env-file=./.env ^
+    --name hxe ^
+    --hostname hxe ^
+    -v %1:/hana/mounts ^
+    -p 39017:39017 ^
+    -p 39041:39041 ^
+    -p 39013:39013 ^
+    -p 39015:39015 ^
+    -p 8090:8090 ^ 
+    store/saplabs/hanaexpress:2.00.036.00.20190223.1
